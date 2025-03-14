59783a29db49c3f2ef2e6a421cb387d5a54fc0ec
widya Yasa <widyarusmananda15@gmail.com>
2025-03-14 15:58:31 +0800

    feat: order store

---
+++ src/stores/orderStore.ts
@@ -0,0 +1,129 @@
+import {create} from "zustand/react";
+import {Order, OrderState, OrderAction} from "@/types/Order";
+import {supabase, supabaseClient} from "@/utils/supabase/client";
+import {toast} from "sonner";
+import {redirect} from "next/navigation";
+export const orderStore = create<OrderState & OrderAction>((set, get) => ({
+    orders: [],
+    order: {
+        order_id: "",
+        date: "",
+        customer_name: "",
+        total_price: 0,
+        total_income: 0,
+        status: "",
+        code: "",
+        order_details: [{
+            product_id:"",
+            price:0,
+            quantity:0,
+            product_name:""
+        }],
+        order_outcomes: [{
+            price:0,
+            id:"",
+            name:"",
+            description:""
+        }]
+    },
+    search: "",
+    page: 1,
+    totalData: 0,
+    loading: false,
+    loadingCrud: false,
+    loadingDetail: false,
+    error: "",
+    status: 0,
+    id: "",
+    message: '',
+    changeStatus: (status) => set(() => ({status : status})),
+    getOrder: async (searchData?:string, pageSelected?:number) => {
+        set ({
+            loading:true,
+            search: searchData || '',
+            page: pageSelected || 1
+        })
+        const {search, page} = get()
+        const {data, count} = await supabase
+            .from("orderstatus")
+            .select('*', {count:'exact'})
+            .range((page - 1)*7 , page * 7 - 1)
+            .like('date', `%${search}%`)
+            .like('customer_name', `%${search}%`)
+            .like('status', `%${search}%`)
+        set({
+            orders: data ?? [],
+            totalData: count ?? 0
+        })
+        set({
+            search: '',
+            page: 1,
+            loading: false
+        })
+    },
+    getOrderById: async (id:string) => {
+        set({loadingDetail:true})
+        const {data} = await supabaseClient
+            .from('orderstatus')
+            .select()
+            .eq('order_id', id)
+        if (data) {
+            set({order: data[0]})
+            set({loadingDetail:false})
+        }
+    },
+    createOrder: async (req:Order) => {
+        set({loadingCrud:true})
+        const {status, error} = await supabaseClient
+            .rpc('create_order', {
+                ...req
+            })
+        get().getOrder()
+        set({status: status})
+        set({loadingCrud: false})
+
+        if (status == 201) {
+            set({message: 'Create Order Success'})
+            toast.success(get().message)
+            redirect('/dashboard/order')
+        }
+        if (error?.code == '23505') {
+            toast.warning('Order is Already Exists')
+        } else {
+            console.log(error)
+        }
+    },
+    updateOrder: async (id:string, req:Order) => {
+        set({loadingCrud:true})
+        const {status, error} = await supabaseClient
+            .rpc('update_order', {
+                p_order_id: id,
+                ...req
+            })
+        get().getOrder()
+        set({status: status})
+        set({loadingCrud:false})
+        if (status == 204) {
+            set({message: 'Update Order Success'})
+            toast.success(get().message)
+            redirect('/dashboard/order')
+        }
+        if (error?.code == '23505') {
+            toast.warning('Order is Already Exists')
+        } else {
+            console.log(error)
+        }
+    },
+    deleteOrder: async (id:string) => {
+        set({loadingCrud:true})
+        const {status} = await supabaseClient
+            .rpc('delete_order', { order_uuid: id });
+        get().getOrder()
+        set({status: status})
+        if (status == 204) {
+            set({message: 'Delete Order Success'})
+            toast.success(get().message)
+        }
+        set({loadingCrud:false})
+    }
+}))
--- src/types/Order.ts
+++ src/types/Order.ts
@@ -1,9 +1,10 @@
+
 export type Order = {
     order_id: string
     date: string
     customer_name: string
     total_price: number
-    total_outcome: number
+    total_outcome?: number
     total_income: number
     status: string
     code: string
@@ -12,7 +13,7 @@ export type Order = {
             product_id: string
             product_name: string
             quantity: number
-            price: string
+            price: number
         }
     ]
     order_outcomes: [
@@ -23,4 +24,27 @@ export type Order = {
             price:number
         }
     ]
+}
+
+export type OrderState = {
+    orders: Order[]
+    order: Order
+    search: string
+    page: number
+    totalData: number
+    loading: boolean
+    loadingCrud: boolean
+    loadingDetail: boolean
+    error: string
+    status: number
+    id: string
+    message:string
+}
+export type OrderAction = {
+    changeStatus: (status: number) => void
+    getOrder: (search?:string, page?:number) => void
+    getOrderById: (id: string) => void
+    createOrder: (data:Order) => void
+    updateOrder: (id: string, data:Order) => void
+    deleteOrder: (id: string) => void
 }
