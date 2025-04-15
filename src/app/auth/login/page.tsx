cacb66ac660048dff2a8f3b8b58938d9be2e38e2
widya Yasa <widyarusmananda15@gmail.com>
2025-04-15 18:36:06 +0800

    feat: add login form

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,56 +1,12 @@
-'use client'
-
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
-import { useRouter } from 'next/navigation'
-import { useState } from 'react'
-import {Input} from "@/components/ui/input";
-import {Label} from "@/components/ui/label";
-import {Button} from "@/components/ui/button";
-import {toast} from "sonner";
+import LoginForm from "@/components/dashboard/forms/login";
 
 export default function Login() {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const router = useRouter()
-    const supabase = createClientComponentClient()
-
-    const handleSignIn = async () => {
-        const login = await supabase.auth.signInWithPassword({
-            email,
-            password,
-        })
-        if (login.error !== null) {
-            toast.error('Login failed, please check your email and password')
-        } else {
-            toast.success('Login success')
-            router.push('/dashboard')
-        }
-    }
-
-    // const handleSignOut = async () => {
-    //     await supabase.auth.signOut()
-    //     router.refresh()
-    // }
-
     return (
         <>
             <div className="w-full flex h-screen justify-center items-center">
                 <div className="flex flex-col w-[600px] p-10 border rounded-lg">
                     <h1 className="text-center text-4xl font-semibold">Login Page</h1>
-                    <div className="mt-5">
-                        <Label htmlFor="email">Email</Label>
-                        <Input type={"email"} name="email" onChange={(e) => setEmail(e.target.value)} value={email}/>
-                    </div>
-                    <div className="mt-3">
-                        <Label htmlFor="password">Password</Label>
-                        <Input
-                            type="password"
-                            name="password"
-                            onChange={(e) => setPassword(e.target.value)}
-                            value={password}
-                        />
-                    </div>
-                    <Button className={"mt-10"} onClick={handleSignIn}>Sign in</Button>
+                    <LoginForm />
                 </div>
             </div>
         </>
---
+++ src/components/dashboard/forms/login.tsx
@@ -0,0 +1,45 @@
+import {Label} from "@/components/ui/label";
+import {Input} from "@/components/ui/input";
+import {Button} from "@/components/ui/button";
+import {useState} from "react";
+import {useRouter} from "next/navigation";
+import {createClientComponentClient} from "@supabase/auth-helpers-nextjs";
+import {toast} from "sonner";
+
+export default function LoginForm() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const router = useRouter()
+    const supabase = createClientComponentClient()
+
+    const handleSignIn = async () => {
+        const login = await supabase.auth.signInWithPassword({
+            email,
+            password,
+        })
+        if (login.error !== null) {
+            toast.error('Login failed, please check your email and password')
+        } else {
+            toast.success('Login success')
+            router.push('/dashboard')
+        }
+    }
+    return (
+        <>
+            <div className="mt-5">
+                <Label htmlFor="email">Email</Label>
+                <Input type={"email"} name="email" onChange={(e) => setEmail(e.target.value)} value={email}/>
+            </div>
+            <div className="mt-3">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                    type="password"
+                    name="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                />
+            </div>
+            <Button className={"mt-10"} onClick={handleSignIn}>Sign in</Button>
+        </>
+    )
+}
